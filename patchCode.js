259b25cbb274c90eb7a83f650827cc999077acd6
灯、等灯等灯 <350030173@qq.com>
2024-01-25 15:56:52 +0800

    新增获取unity版本

--- patchCode.js
+++ patchCode.js
@@ -1,4 +1,75 @@
 
+//获取unity版本
+function il2cpp_Version()
+{
+	var soName = "libil2cpp.so";
+    var il2cpp_get_corlib_ptr = Module.getExportByName(soName, 'il2cpp_get_corlib');
+    var il2cpp_domain_get_ptr = Module.getExportByName(soName, 'il2cpp_domain_get');
+    var il2cpp_domain_get_assemblies_ptr = Module.getExportByName(soName, 'il2cpp_domain_get_assemblies');
+    var il2cpp_assembly_get_image_ptr = Module.getExportByName(soName, 'il2cpp_assembly_get_image');
+    var il2cpp_class_from_name_ptr = Module.getExportByName(soName, 'il2cpp_class_from_name');
+    var il2cpp_class_get_method_from_name_ptr = Module.getExportByName(soName, 'il2cpp_class_get_method_from_name');
+    var il2cpp_image_get_name_ptr = Module.getExportByName(soName, 'il2cpp_image_get_name');
+    var il2cpp_image_get_class_count_ptr = Module.getExportByName(soName, 'il2cpp_image_get_class_count');
+    var il2cpp_string_new_ptr = Module.getExportByName(soName, 'il2cpp_string_new');
+
+    var il2cpp_get_corlib = new NativeFunction(il2cpp_get_corlib_ptr, "pointer", []);
+    var il2cpp_domain_get_assemblies = new NativeFunction(il2cpp_domain_get_assemblies_ptr, 'pointer', ['pointer', 'pointer']);
+    var il2cpp_assembly_get_image = new NativeFunction(il2cpp_assembly_get_image_ptr, 'pointer', ['pointer']);
+    var il2cpp_class_from_name = new NativeFunction(il2cpp_class_from_name_ptr, "pointer", ["pointer", "pointer", "pointer"]);
+    var il2cpp_class_get_method_from_name = new NativeFunction(il2cpp_class_get_method_from_name_ptr, "pointer", ["pointer", "pointer", "int"]);
+    var il2cpp_image_get_name = new NativeFunction(il2cpp_image_get_name_ptr, "pointer", ["pointer"]);
+    var il2cpp_image_get_class_count = new NativeFunction(il2cpp_image_get_class_count_ptr, "pointer", ["pointer"]);
+    var il2cpp_string_new = new NativeFunction(il2cpp_string_new_ptr, "pointer", ["pointer"]);
+
+    //存放所有的dll的指针
+    const all_dll_ptr = Memory.alloc(Process.pointerSize * 100);
+    var assemblies = il2cpp_domain_get_assemblies(il2cpp_domain_get_ptr, all_dll_ptr);
+
+    var UnityEngine_str = Memory.allocUtf8String("UnityEngine");
+    var Application_str = Memory.allocUtf8String("Application");
+	var get_unityVersion_str = Memory.allocUtf8String("get_unityVersion");
+
+    for (let index = 0; index < 100; index++)
+    {
+        //console.log("index== "+assemblies.add(Process.pointerSize*index));
+        try
+        {
+            var dll_ptr = il2cpp_assembly_get_image(assemblies.add(Process.pointerSize * index)).readPointer();
+        } catch (error)
+        {
+			return;
+        }
+
+        //console.log(JSON.stringify("dll_ptr :"+dll_ptr, null, 4));
+        if (dll_ptr >= 0x100000)
+        {
+            var dll = il2cpp_image_get_name(dll_ptr);
+            //console.log(dll.readUtf8String());
+            if (dll.readUtf8String() == "UnityEngine.CoreModule.dll")
+            {
+                //console.log(dll.readUtf8String());
+                //var classCount = il2cpp_image_get_class_count(dll_ptr);
+                var Application = il2cpp_class_from_name(dll_ptr, UnityEngine_str, Application_str)
+
+                try
+                {
+                    var get_unityVersion = il2cpp_class_get_method_from_name(Application, get_unityVersion_str, 0);
+                    var get_unityVersion_ptr = new NativeFunction(get_unityVersion.readPointer(), 'pointer', []);
+					
+					//主动调用 get_unityVersion 方法
+                    console.log("\n unity版本:\n " + get_unityVersion_ptr().add(20).readUtf16String()+"\n");
+                } catch (error)
+                {
+                    console.log("无法获取 unity 版本，需手动查找");
+                }
+
+            }
+        }
+    }
+}
+
+
 /**
 
 内存指令替换，支持ARM 32位，64位
@@ -24,9 +95,11 @@ function reverseHex(hexString)
 /**
  * args[0]: 目标so
  * args[1]: 目标地址
- * args[2]，args[2]，args[x]: 改变的16进制码,可以有多个
+ * args[2]，args[2]，args[x]: 要改变的16进制码,可以有多个,指令大小写均可
  * 
- * 例:patchCode("libil2cpp.so",0x601560,"01 00 a0 e3","1E FF 2F E1")
+ * 例:
+ * patchCode("libil2cpp.so",0x601560,"01 00 a0 e3","1E FF 2F E1")
+ * patchCode("libil2cpp.so",0x711340,"C0 03 5F D6")
  * 
  * @param  {...any} args 
  */
@@ -39,7 +112,7 @@ function patchCode(...args)
         var nativePointer = Module.getBaseAddress(args[0]).add(args[1]);
         Memory.patchCode(nativePointer, 4, function (code) 
         {
-            console.log("before======================");
+            console.log("\n\n改之前======================");
             console.log(hexdump(nativePointer, { length: 0x50 }));
 
             if (Process.arch == "arm64")
@@ -60,7 +133,7 @@ function patchCode(...args)
                 armWt.flush();
             }
 
-            console.log("\nafter======================");
+            console.log("\n\n改之后======================");
             console.log(hexdump(nativePointer, { length: 0x50 }));
         });
     } else
@@ -73,7 +146,38 @@ function patchCode(...args)
 
 function main()
 {
+	il2cpp_Version();
+	/**
+	
+	常用指令：
+	==============ARM32=============
+	NOP                 (00 F0 20 E3)
+	MOV    r0,#0        (00 00 A0 E3)
+	MOV    r0,#1        (01 00 A0 E3)
+	MOV    r0,#100      (64 00 A0 E3)
+	MOV    r0,#0xffffff (FF 04 E0 E3)
+	BX     LR           (1E FF 2F E1)
+	================================
+	
+	==============ARM64=============
+	NOP                 (1F 20 03 D5)
+	MOV    X0,#0        (00 00 80 D2)
+	MOV    X0,#1        (20 00 80 D2)
+	MOV    X0,#100      (80 0C 80 D2)
+	MOV    X0,#0xffffff (E0 5F 40 B2)
+	RET                 (C0 03 5F D6)
+	================================
+	*/
     patchCode("libil2cpp.so", 0x14a126c, "20 00 80 52", "C0 03 5F D6");
 }
 
+//setTimeout(main, 3000);
 setImmediate(main);
+
+
+
+
+
+
+
+
